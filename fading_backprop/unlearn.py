cf3adfa6166e1dbfae8a41e4743e60727ca1dcdc
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-10-11 19:04:19 +0200

    make lang configurable

--- fading_backprop/unlearn.py
+++ fading_backprop/unlearn.py
@@ -29,39 +29,47 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=pt.bfloat16,
 ).to(device)
 
+
 # %% load dataset
-# only use one ~600MB shard; also, streaming would make splitting too slow
-oscar_tmpl = "hf://datasets/oscar-corpus/OSCAR-2301/{lang}_meta/{lang}_meta_part_1.jsonl.zst"  # fmt: skip
-raw_dataset = load_dataset("text", data_files=oscar_tmpl.format(lang="pl"))
-raw_dataset = raw_dataset["train"]  # train is the only split
-
-# split into 4 quarters
-half1, half2 = raw_dataset.train_test_split(test_size=0.5, seed=42).values()
-quarter1, quarter2 = half1.train_test_split(test_size=0.5, seed=42).values()
-quarter3, quarter4 = half2.train_test_split(test_size=0.5, seed=42).values()
-
-dataset = (
-    # define splits; make it iterable so that it can be processed on demand
-    IterableDatasetDict(
-        unlearn=IterableDataset.from_generator(lambda: (ex for ex in quarter1)),
-        relearn=IterableDataset.from_generator(lambda: (ex for ex in quarter2)),
-        validation=IterableDataset.from_generator(lambda: (ex for ex in quarter3)),
-        test=IterableDataset.from_generator(lambda: (ex for ex in quarter4)),
+def load_one_oscar_shard(lang):
+    # only use one ~600MB shard
+    # also, streaming would make splitting too slow
+    raw_dataset = load_dataset(
+        "text",
+        split="train",  # train is the only split in oscar
+        data_files=f"hf://datasets/oscar-corpus/OSCAR-2301/{lang}_meta/{lang}_meta_part_1.jsonl.zst",
     )
-    # process the raw data, following OSCAR-2301.py
-    .map(lambda ex: {"text": json.loads(ex["text"])["content"]})
-    # tokenize
-    .map(
-        lambda ex: tokenizer(
-            ex["text"],
-            return_tensors="pt",
-            max_length=context_len,
-            truncation=True,
-        ).to(device),
+
+    # split into 4 quarters
+    half1, half2 = raw_dataset.train_test_split(test_size=0.5, seed=42).values()
+    quarter1, quarter2 = half1.train_test_split(test_size=0.5, seed=42).values()
+    quarter3, quarter4 = half2.train_test_split(test_size=0.5, seed=42).values()
+
+    return (
+        # define splits; make it iterable so that it can be processed on demand
+        IterableDatasetDict(
+            unlearn=IterableDataset.from_generator(lambda: (ex for ex in quarter1)),
+            relearn=IterableDataset.from_generator(lambda: (ex for ex in quarter2)),
+            validation=IterableDataset.from_generator(lambda: (ex for ex in quarter3)),
+            test=IterableDataset.from_generator(lambda: (ex for ex in quarter4)),
+        )
+        # process the raw data, following OSCAR-2301.py
+        .map(lambda ex: {"text": json.loads(ex["text"])["content"]})
+        # tokenize
+        .map(
+            lambda ex: tokenizer(
+                ex["text"],
+                return_tensors="pt",
+                max_length=context_len,
+                truncation=True,
+            ).to(device),
+        )
+        # filter out the short ones
+        .filter(lambda ex: ex["input_ids"].shape[-1] >= context_len)
     )
-    # filter out the short ones
-    .filter(lambda ex: ex["input_ids"].shape[-1] >= context_len)
-)
+
+
+dataset = load_one_oscar_shard(lang="pl")
 
 # %%
 # optimizer = pt.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
@@ -69,7 +77,6 @@ optimizer = pt.optim.SGD(model.parameters(), lr=0.0001)
 optimizer.zero_grad()
 loss_fn = pt.nn.CrossEntropyLoss()
 
-# %%
 for batch in dataset["unlearn"].batch(batch_size, drop_last_batch=True).take(10):
     # create batched input_ids
     input_ids = pt.cat(batch["input_ids"])
