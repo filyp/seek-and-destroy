3c1ece96520beb3a988f73f7ad98f780dda22cd5
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-10-18 12:53:23 +0200

    reorganize files, comment

--- fading_backprop/fading_backprop.py
+++ fading_backprop/fading_backprop.py
@@ -1,34 +1,54 @@
 # %%
-from itertools import islice
-
-import matplotlib.pyplot as plt
 import torch as pt
 import wandb
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from utils import device, forward, get_perplexity, load_one_oscar_shard
 
-# model_id = "google/gemma-2-2b"
-model_id = "Qwen/Qwen2.5-0.5B"
-
-# %% load dataset
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-pl_dataset = load_one_oscar_shard("pl", tokenizer)
-en_dataset = load_one_oscar_shard("en", tokenizer)
-
 
 # %%
+def install_hooks_for_saving_gradients(model):
+    # save the gradients downstream of each mlp, to be used in custom unlearning
+    def save_output_grad_hook(module, grad_input, grad_output):
+        module.output_grad = grad_output[0]
+
+    for layer in model.model.layers:
+        assert not layer.mlp.down_proj._backward_hooks
+        layer.mlp.down_proj.register_full_backward_hook(save_output_grad_hook)
+
+
+def install_hooks_for_fading_backprop(model):
+    # (tested for gemma-2-2b and Qwen2.5-0.5B)
+    def scale_grad_hook(module, grad_input, grad_output):
+        grad = list(grad_input)
+        # we rely on fade_factor set with set_fade_factor
+        grad[0] *= module.fade_factor
+        return grad
+
+    for layer in model.model.layers:
+        assert not layer.mlp._backward_hooks
+        layer.mlp.register_full_backward_hook(scale_grad_hook)
+        assert not layer.input_layernorm._backward_hooks
+        layer.input_layernorm.register_full_backward_hook(scale_grad_hook)
+
+
+def set_fade_factor(model, fade_factor):
+    # saves the fade_factor in each relevant module, so that hooks can access it
+    for layer in model.model.layers:
+        layer.mlp.fade_factor = fade_factor
+        layer.input_layernorm.fade_factor = fade_factor
+
+
 def normal_train_step(model, batch, lr):
+    set_fade_factor(model, 1)
     optimizer = pt.optim.SGD(model.parameters(), lr=lr)
     optimizer.zero_grad(set_to_none=True)
-
     loss = forward(model, batch)
     loss.backward()
-
     optimizer.step()
-    optimizer.zero_grad(set_to_none=True)  # unneeded, but clean up just to be sure
 
 
-def unlearn_step(model, batch, lr):
+def unlearn_step(model, batch, lr, fade_factor):
+    set_fade_factor(model, fade_factor)
     optimizer = pt.optim.SGD(model.parameters(), lr=lr)
 
     loss = forward(model, batch)
@@ -39,7 +59,7 @@ def unlearn_step(model, batch, lr):
     # calculate custom grads
     for layer in model.model.layers:
         module = layer.mlp.down_proj
-        # get the grad
+        # get the downstream gradient saved by the hook
         output_grad = module.output_grad
         output_grad = output_grad[:, :-1, :]  # last token position has no gradient
         assert output_grad.norm(dim=-1).all()
@@ -51,13 +71,6 @@ def unlearn_step(model, batch, lr):
         module.weight.grad = update
 
     optimizer.step()
-    optimizer.zero_grad(set_to_none=True)  # unneeded, but clean up just to be sure
-
-
-def set_fade_factor(model, fade_factor):
-    for layer in model.model.layers:
-        layer.mlp.fade_factor = fade_factor
-        layer.input_layernorm.fade_factor = fade_factor
 
 
 # %%
@@ -75,27 +88,8 @@ def unlearn_and_relearn(
     batch_size=32,
 ):
     # todo test for f=0
-
-    # install hooks for saving gradients
-    def save_output_grad_hook(module, grad_input, grad_output):
-        module.output_grad = grad_output[0]
-
-    for layer in model.model.layers:
-        assert not layer.mlp.down_proj._backward_hooks
-        layer.mlp.down_proj.register_full_backward_hook(save_output_grad_hook)
-
-    # install hooks for fading the backprop
-    # (tested for gemma-2-2b and Qwen2.5-0.5B)
-    def scale_grad_hook(module, grad_input, grad_output):
-        grad = list(grad_input)
-        grad[0] *= module.fade_factor
-        return grad
-
-    for layer in model.model.layers:
-        assert not layer.mlp._backward_hooks
-        layer.mlp.register_full_backward_hook(scale_grad_hook)
-        assert not layer.input_layernorm._backward_hooks
-        layer.input_layernorm.register_full_backward_hook(scale_grad_hook)
+    install_hooks_for_saving_gradients(model)
+    install_hooks_for_fading_backprop(model)
 
     # create dataset iterators
     target_unlearn_iter = iter(target_dataset["unlearn"].batch(batch_size))
@@ -103,81 +97,62 @@ def unlearn_and_relearn(
     retain_relearn_iter = iter(retain_dataset["relearn"].batch(batch_size))
 
     # initial perplexities
-    perplexities = {
+    ppl = {
         "target": get_perplexity(model, target_dataset),
         "retain": get_perplexity(model, retain_dataset),
     }
-    print("initial perplexity: ", {k: f"{v:.2f}" for k, v in perplexities.items()})
+    print("initial perplexity: ", {k: f"{v:.2f}" for k, v in ppl.items()})
 
     # perplexity on retain set is not allowed to raise above this value
     # - this triggers relearning on retain set
-    allowed_retain_perplexity = perplexities["retain"] * 1.2
+    allowed_retain_perplexity = ppl["retain"] * 1.2
     print(f"{allowed_retain_perplexity=:.2f}")
 
     # unlearning loop
     for step_num in range(num_unlearning_steps):
         print(f"\nstep {step_num + 1:3}", end="  ")
-        if perplexities["retain"] > allowed_retain_perplexity:
+        # retain set perplexity too high, so relearn it
+        if ppl["retain"] > allowed_retain_perplexity:
             print("> relearning retain", end="  ")
-            set_fade_factor(model, 1)
             normal_train_step(model, next(retain_relearn_iter), relearn_lr)
-        elif perplexities["target"] > pl_ppl_threshold:
+        # do a bit of target relearning, to make unlearning more relevant
+        elif ppl["target"] > pl_ppl_threshold:
             print("**relearning target", end="  ")
-            set_fade_factor(model, 1)
             # we intentionally use target_UNlean_iter, to not affect relearn split here
             normal_train_step(model, next(target_unlearn_iter), relearn_lr)
+        # unlearn target
         else:
             print("  unlearning target", end="  ")
-            set_fade_factor(model, f_schedule(step_num))
-            unlearn_step(model, next(target_unlearn_iter), unlearn_lr)
+            fade_factor = f_schedule(step_num)
+            unlearn_step(model, next(target_unlearn_iter), unlearn_lr, fade_factor)
 
+        # evaluate
         if (step_num + 1) % eval_every_n_steps == 0:
-            perplexities = {
+            ppl = {
                 "target": get_perplexity(model, target_dataset),
                 "retain": get_perplexity(model, retain_dataset),
             }
-            print({k: f"{v:.2f}" for k, v in perplexities.items()}, end="  ")
+            print({k: f"{v:.2f}" for k, v in ppl.items()}, end="  ")
 
     # relearning loop
-    print("\n### relearning pl started ###")
-    set_fade_factor(model, 1)
+    print("\n### relearning started ###", end="  ")
     for step_num in range(num_relearning_steps):
         print(f"\nstep {step_num + 1:3}", end="  ")
-        if perplexities["retain"] > allowed_retain_perplexity:
+        if ppl["retain"] > allowed_retain_perplexity:
             print("> relearning retain", end="  ")
             normal_train_step(model, next(retain_relearn_iter), relearn_lr)
         else:
             print("  relearning target", end="  ")
             normal_train_step(model, next(target_relearn_iter), relearn_lr)
 
+        # evaluate
         if (step_num + 1) % eval_every_n_steps == 0:
-            perplexities = {
+            ppl = {
                 "target": get_perplexity(model, target_dataset),
                 "retain": get_perplexity(model, retain_dataset),
             }
-            print({k: f"{v:.2f}" for k, v in perplexities.items()}, end="  ")
+            print({k: f"{v:.2f}" for k, v in ppl.items()}, end="  ")
+    print("\n###### relearning finished ######")
 
-    return perplexities
+    return ppl
 
-
-# %%
-criterion = min
-param_name = "relearn_lr"
-param_starting_value = 0.001
-param_value_and_ppl = []
-
-def get_final_target_perplexity(**kwargs):
-    # load model
-    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=pt.bfloat16)
-    model.to(device)
-
-    final_perplexities = unlearn_and_relearn(model, pl_dataset, en_dataset, **kwargs)
-    return final_perplexities["target"]
-
-
-# %%
-ppl = get_final_target_perplexity(**{param_name: param_starting_value})
-param_value_and_ppl.append((param_starting_value, ppl))
-ppl = get_final_target_perplexity(**{param_name: param_starting_value * 2})
-param_value_and_ppl.append((param_starting_value * 2, ppl))
-# %%
---
+++ fading_backprop/param_search.py
@@ -0,0 +1,45 @@
+
+import matplotlib.pyplot as plt
+import torch as pt
+import wandb
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from utils import device, load_one_oscar_shard
+from fading_backprop import unlearn_and_relearn
+
+
+# %% load dataset
+# model_id = "google/gemma-2-2b"
+model_id = "Qwen/Qwen2.5-0.5B"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+pl_dataset = load_one_oscar_shard("pl", tokenizer)
+en_dataset = load_one_oscar_shard("en", tokenizer)
+
+
+# %%
+criterion = min
+param_name = "relearn_lr"
+param_starting_value = 0.001
+param_value_and_ppl = []
+
+
+def get_final_target_perplexity(**kwargs):
+    # load model
+    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=pt.bfloat16)
+    model.to(device)
+
+    final_perplexities = unlearn_and_relearn(model, pl_dataset, en_dataset, **kwargs)
+    return final_perplexities["target"]
+
+
+# # %%
+# ppl = get_final_target_perplexity(**{param_name: param_starting_value})
+# param_value_and_ppl.append((param_starting_value, ppl))
+# ppl = get_final_target_perplexity(**{param_name: param_starting_value * 2})
+# param_value_and_ppl.append((param_starting_value * 2, ppl))
+# # %%
+
+model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=pt.bfloat16)
+model.to(device)
+
+final_perplexities = unlearn_and_relearn(model, pl_dataset, en_dataset)
