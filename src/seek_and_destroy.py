0aece952ad15b0eb474ce7dab9fd8681a2232ecf
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-12-01 20:31:31 +0100

    no retain lora

--- src/seek_and_destroy.py
+++ src/seek_and_destroy.py
@@ -17,20 +17,17 @@ config = SimpleNamespace(
     # Model/data configs
     model_id="EleutherAI/pythia-14m",
     forget_set_name="python",
-    ret_lora_config=dict(
-        # lora_dropout=0.1,
-        target_modules=["dense_h_to_4h", "dense_4h_to_h", "query_key_value", "dense"],
-    ),
+    ret_lora_config=dict(lora_dropout=0.05, target_modules="all-linear"),
     # Training constants
-    unlearn_steps=300,
+    # unlearn_steps=50,
     batch_size=16,
     # Relearning params
-    relearn_steps=100,
+    relearn_steps=50,
     relearn_lr=3e-4,
     eval_batch_size=16,
-    relearn_lora_conf=dict(r=1, target_modules="all-linear", lora_dropout=0.1),
+    relearn_lora_conf=dict(r=1, target_modules="all-linear", lora_dropout=0.05),
     # Default tunable params
-    disruption_score_warmup=10,
+    disruption_score_warmup=20,
 )
 
 pt.set_default_device("cuda")
@@ -57,9 +54,6 @@ init_forget = eval_loss(base_model, f_eval_batch)
 init_retain = eval_loss(base_model, r_eval_batch)
 logging.info(f"init forget: {init_forget:6.2f}    init retain: {init_retain:6.2f}")
 
-best_model_path = repo_root() / "models" / "best_model.pt"
-best_value = 0
-
 _circuit_name = f"{config.forget_set_name}_correct_logit.pt"
 circuit = pt.load(repo_root() / "circuits" / config.model_id / _circuit_name)
 
@@ -69,51 +63,26 @@ def objective(trial):
     global best_value
     # ! parameters
     quantile = trial.suggest_float("quantile", 0.001, 0.05, log=True)
-    ret_lora_lr = trial.suggest_float("ret_lora_lr", 1e-4, 5e-4, log=True)
-    unlearn_lr = trial.suggest_float("unlearn_lr", 0.01, 0.2, log=True)
-    unlearn_lr_mult = 1 # trial.suggest_float("unlearn_lr_mult", 0.99, 1.01)
-    forget_amp = trial.suggest_float("forget_amp", 1, 1.2)
+    unlearn_lr = trial.suggest_float("unlearn_lr", 0.001, 0.1, log=True)
     retain_amp = trial.suggest_float("retain_amp", 1, 1.4)
-    ret_lora_rank = trial.suggest_int("ret_lora_rank", 1, 5)
-    ret_lora_dropout = trial.suggest_float("ret_lora_dropout", 0.0, 0.1)
-
+    forget_amp = trial.suggest_float("forget_amp", 1, 1.2)
     disruption_score_decay = trial.suggest_float("disruption_score_decay", 0.0, 0.95)
-    # disruption_score_warmup = trial.suggest_int("disruption_score_warmup", 1, 20)
+    unlearn_steps = trial.suggest_int("unlearn_steps", 30, 200, step=10)
 
-    unlearn_lr_backoff = trial.suggest_float("unlearn_lr_backoff", 0.9, 1)
+    ret_lora_lr = 0  #trial.suggest_float("ret_lora_lr", 1e-4, 5e-4, log=True)
+    ret_lora_rank = 3  # trial.suggest_int("ret_lora_rank", 1, 5)
 
-    # target_modules = ["dense_4h_to_h", "dense"]  # for python keep these two
-    # decide which modules to attack
-    target_modules = []
-    if trial.suggest_categorical("mod_down_proj", [True, False]):
-        target_modules.append("dense_4h_to_h")
-    if trial.suggest_categorical("mod_up_proj", [True, False]):
-        target_modules.append("dense_h_to_4h")
-    if trial.suggest_categorical("mod_attn", [True, False]):
-        target_modules.append("query_key_value")
-    if trial.suggest_categorical("mod_attn_out", [True, False]):
-        target_modules.append("dense")
-    if not target_modules:
-        raise optuna.TrialPruned()
-
-    #     logging.info(f"{trial.params}")
-
-    # set_seeds(42)  # note: something is still undeterministic!
     # prepare data iterators
-    forget_iter = forget_batches.fresh_iterator()
     retain_iter = retain_batches.fresh_iterator()
-
     # load model (copy from memory for speed)
-    # note: to save memory you may want to load from_pretrained instead
     model = deepcopy(base_model)
 
     # add lora
-    ret_lora_c = LoraConfig(
-        r=ret_lora_rank, lora_dropout=ret_lora_dropout, **config.ret_lora_config
-    )
+    ret_lora_c = LoraConfig(r=ret_lora_rank, **config.ret_lora_config)
     peft_model = get_peft_model(model, ret_lora_c, adapter_name="ret_lora", mixed=True)
     model = peft_model.model
 
+    target_modules = ["dense_4h_to_h", "dense"]  # for python keep these two
     interven_params = [
         p
         for n, p in model.named_parameters()
@@ -122,10 +91,8 @@ def objective(trial):
     ret_lora_params = [p for n, p in model.named_parameters() if ".ret_lora." in n]
 
     # initialize optimizers
-    # SGD is faster and more predictable than Adam
     base_optimizer = pt.optim.SGD(interven_params, lr=unlearn_lr)
     ret_optimizer = pt.optim.SGD(ret_lora_params, lr=ret_lora_lr)
-
     # initialize disruption scores
     for param in interven_params:
         param.disruption_score = pt.zeros_like(param)
@@ -140,18 +107,14 @@ def objective(trial):
             p.to_forget = circuit[n]
     assert all(p.to_forget.shape == p.shape for p in interven_params)
 
-    # %
     # ! unlearning loop
     res = {}
     logging.info("step      base_f      base_r")
-    _steps_with_broken_retain = 0
-    for step in range(1, 1 + config.unlearn_steps):
+    for step in range(1, 1 + unlearn_steps):
         model.train()
-        f_input_ids = next(forget_iter)
         r_input_ids = next(retain_iter)
 
         # ! retain with helper lora
-        peft_model.set_adapter(["ret_lora"])
         only_grad_on(model, interven_params + ret_lora_params)
         model.zero_grad(set_to_none=True)
         loss = loss_fns["cross_entropy"](model(r_input_ids), r_input_ids)
@@ -166,77 +129,43 @@ def objective(trial):
 
         # ! unlearn on the base model
         if res.get("base_retain", 0) < init_retain + 0.1:
-            _steps_with_broken_retain = 0
-            base_optimizer.param_groups[0]["lr"] *= unlearn_lr_mult
-
-            # ! copy gradients
             model.zero_grad(set_to_none=True)
-            for p in interven_params:
-                p.grad = deepcopy(p.to_forget)
-
             # ! get threshold
             final_scores = [mask_fn(p) for p in interven_params]
             threshold = get_threshold(quantile, final_scores)
             # ! apply mask
             for param in interven_params:
                 mask = mask_fn(param) < threshold
-                param.grad *= mask
-            # ! normalize gradients
-            grad_norm = sum(p.grad.norm() ** 2 for p in interven_params) ** 0.5
-            for p in interven_params:
-                p.grad /= grad_norm
+                param.grad = mask * param.to_forget
             base_optimizer.step()
-        else:
-            _steps_with_broken_retain += 1
-            base_optimizer.param_groups[0]["lr"] *= unlearn_lr_backoff
-            if step % 10 == 1:
-                logging.info(f"step {step} - broken retain")
-            if _steps_with_broken_retain > 50:
-                logging.error("Retain performance broken for 50 steps")
-                trial.set_user_attr("retain_broken", True)
-                raise optuna.TrialPruned()
-
 
         # ! eval
         if step % 10 == 0:
             res = {}
-            peft_model.set_adapter(["ret_lora"])
             res["base_forget"] = eval_loss(model, f_eval_batch)
             res["base_retain"] = eval_loss(model, r_eval_batch)
 
             logging.info(f"{step:4} " + " ".join(f"{v:11.2f}" for v in res.values()))
 
-            # prune if base forget loss doesn't improve
-            if step >= 30 and res["base_forget"] < init_forget + 0.5:
-                logging.info("Forget loss stalled")
-                raise optuna.TrialPruned()
             # prune if nan
             if any(pt.isnan(v) for v in res.values()):
                 logging.error("NaN in eval results")
                 raise optuna.TrialPruned()
+            if res["base_retain"] > init_retain + 0.1:
+                logging.info("Retain performance broken")
+                raise optuna.TrialPruned()
 
-    if res["base_retain"] > init_retain + 0.1:
-        logging.info("Retain performance still broken")
-        raise optuna.TrialPruned()
-
-    # %
     # ! final bigger eval relearning
     collapsed_model = copy_model_and_collapse_loras(peft_model, delete_adv=False)
     retain_val_iter = retain_val_batches.fresh_iterator()
     forget_val_iter = forget_val_batches.fresh_iterator()
     forget_loss = relearn(collapsed_model, config, retain_val_iter, forget_val_iter)
-    # save best model
-    if forget_loss > best_value:
-        logging.info(f"New best model with forget loss {forget_loss}")
-        best_value = forget_loss
-        collapsed_model = copy_model_and_collapse_loras(peft_model, delete_adv=False)
-        pt.save(collapsed_model.state_dict(), best_model_path)
     return forget_loss
 
 
 # %%
 info = f"S&D,{config.forget_set_name},{config.relearn_steps}rs"
-study_name = f"{info},300us,norm,prune_broken_retain,better_ranges"
+study_name = f"{info},no_retain_lora"
 if __name__ == "__main__":
     assert is_repo_clean()
     study = optuna.create_study(
