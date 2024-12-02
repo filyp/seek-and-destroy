9691115815dc5223599c13edf8faca0c4b1db2d4
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-12-02 19:17:57 +0100

    try pruning

--- src/seek_and_destroy.py
+++ src/seek_and_destroy.py
@@ -18,13 +18,13 @@ config = SimpleNamespace(
     model_id="EleutherAI/pythia-14m",
     forget_set_name="python",
     # Training constants
-    unlearn_steps=100,
+    unlearn_steps=200,
     batch_size=16,
     # Relearning params
-    relearn_steps=50,
-    relearn_lr=3e-4,
+    relearn_steps=20,
+    relearn_lr=1e-4,
     eval_batch_size=16,
-    relearn_lora_conf=dict(r=1, target_modules="all-linear", lora_dropout=0.05),
+    relearn_lora_conf=dict(r=3, target_modules="all-linear", lora_dropout=0.05),
     # Default tunable params
     disruption_score_warmup=10,
 )
@@ -60,11 +60,13 @@ circuit = pt.load(repo_root() / "circuits" / config.model_id / _circuit_name)
 # %%
 def objective(trial):
     # ! parameters
-    quantile = trial.suggest_float("quantile", 0.0003, 0.003, log=True)
-    unlearn_lr = trial.suggest_float("unlearn_lr", 0.0003, 0.003, log=True)
-    retain_amp = trial.suggest_float("retain_amp", 1, 1.4)
+    quantile = trial.suggest_float("quantile", 0.0005, 0.003, log=True)
+    unlearning_rate = trial.suggest_float("unlearning_rate", 0.0003, 0.003, log=True)
+    relearning_rate = trial.suggest_float("relearning_rate", 0.0001, 0.001, log=True)
+
+    retain_amp = trial.suggest_float("retain_amp", 1.2, 1.4)
     forget_amp = trial.suggest_float("forget_amp", 1, 1.2)
-    disruption_score_decay = trial.suggest_float("disruption_score_decay", 0.0, 0.95)
+    disruption_score_decay = trial.suggest_float("disruption_score_decay", 0.0, 0.9)
 
     # prepare data iterators
     retain_iter = retain_batches.fresh_iterator()
@@ -82,7 +84,7 @@ def objective(trial):
             param.to_forget = circuit[name]
 
     # initialize optimizers
-    optimizer = pt.optim.SGD(interven_params, lr=unlearn_lr)
+    optimizer = pt.optim.SGD(interven_params)
     # initialize mask
     mask_fn = lambda param: param.disruption_score / param.to_forget.abs() ** forget_amp
 
@@ -102,45 +104,51 @@ def objective(trial):
             param.disruption_score += param.grad.abs() ** retain_amp
         if step <= config.disruption_score_warmup:
             continue
-
-        # ! unlearn on the base model
-        # get threshold
-        final_scores = [mask_fn(p) for p in interven_params]
-        threshold = get_threshold(quantile, final_scores)
-        # apply mask
-        model.zero_grad(set_to_none=True)
-        for param in interven_params:
-            mask = mask_fn(param) < threshold
-            param.grad = mask * param.to_forget
+        optimizer.param_groups[0]["lr"] = relearning_rate
         optimizer.step()
 
-        # ! eval
+        if res.get("retain_loss_acceptable", True):
+            # it it's unacceptable, we only retain, not unlearn
+            # ! unlearn on the base model
+            # get threshold
+            final_scores = [mask_fn(p) for p in interven_params]
+            threshold = get_threshold(quantile, final_scores)
+            # apply mask
+            model.zero_grad(set_to_none=True)
+            for param in interven_params:
+                mask = mask_fn(param) < threshold
+                param.grad = mask * param.to_forget
+            optimizer.param_groups[0]["lr"] = unlearning_rate
+            optimizer.step()
+
+        # ! eval current loss
         if step % 10 == 0:
-            res = dict(
-                base_forget=eval_loss(model, f_eval_batch),
-                base_retain=eval_loss(model, r_eval_batch),
-            )
+            res = {}
+            res["forget_loss"] = eval_loss(model, f_eval_batch)
+            res["retain_loss"] = eval_loss(model, r_eval_batch)
+            res["retain_loss_acceptable"] = res["retain_loss"] < init_retain + 0.1
             logging.info(f"{step:4} " + " ".join(f"{v:11.2f}" for v in res.values()))
+            assert not any(pt.isnan(v) for v in res.values())
 
-            # prune if nan
-            if any(pt.isnan(v) for v in res.values()):
-                logging.error("NaN in eval results")
-                raise optuna.TrialPruned()
-            if res["base_retain"] > init_retain + 0.1:
-                logging.info("Retain performance broken")
+        # ! eval loss after short relearning
+        if step % 50 == 0:
+            forget_loss = relearn(
+                deepcopy(model),
+                config,
+                retain_val_batches.fresh_iterator(),
+                forget_val_batches.fresh_iterator(),
+            )
+            # prune if trial isn't promising
+            trial.report(forget_loss, step)
+            if trial.should_prune():
                 raise optuna.TrialPruned()
 
-    # ! final bigger eval relearning
-    copied_model = deepcopy(model)
-    retain_val_iter = retain_val_batches.fresh_iterator()
-    forget_val_iter = forget_val_batches.fresh_iterator()
-    forget_loss = relearn(copied_model, config, retain_val_iter, forget_val_iter)
     return forget_loss
 
 
 # %%
 info = f"S&D,{config.forget_set_name},{config.relearn_steps}rs"
-study_name = f"{info},no_retain_lora,better_ranges"
+study_name = f"{info},pruning"
 if __name__ == "__main__":
     assert is_repo_clean()
     study = optuna.create_study(
@@ -148,6 +156,7 @@ if __name__ == "__main__":
         storage=get_storage(),
         direction="maximize",
         # load_if_exists=True,
+        pruner=optuna.pruners.PercentilePruner(75)
     )
     save_script_and_attach_logger(__file__, study.study_name)
     study.set_metric_names(["forget_loss"])
