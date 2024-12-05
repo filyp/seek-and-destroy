fcab9a2e62c7ced0ceb144d25b171e1d2ee1efd4
Filip Sondej <26285707+filyp@users.noreply.github.com>
2024-12-05 21:51:17 +0100

    svg plots

--- src/plots_and_reruns.py
+++ src/plots_and_reruns.py
@@ -1,13 +1,17 @@
 # %%
+from copy import deepcopy
+
+import optuna
 import optuna.visualization as vis
 
-from adversarial_adapters import *
+from utils.git_and_reproducibility import *
+from utils.model_operations import *
 
 # get the latest study
-storage = get_storage()
+# storage = get_storage()
+storage = f"sqlite:///{repo_root() / 'win.sqlite3'}"
 
 # %%
-
 study_summaries = optuna.study.get_all_study_summaries(storage)
 sorted_studies = sorted(study_summaries, key=lambda s: s.datetime_start)
 latest_study = sorted_studies[-1]
@@ -18,51 +22,48 @@ print(study.study_name)
 # study = optuna.load_study(study_name=study_name, storage=storage)
 
 # %%
-# ! plot the slices for each hyperparam
-fig = vis.plot_slice(study, target_name="Final forget loss")
-# params=["quantile", "adv_lora_lr", "ret_lora_lr", "unlearn_lr", "retain_amp"],
-fig.update_layout(
-    title={"text": study.study_name, "xanchor": "center", "x": 0.5, "y": 0.95},
+layout = dict(
     template="plotly_white",
     font=dict(family="Times New Roman", size=20),
+    title={"text": study.study_name, "xanchor": "center", "x": 0.5, "y": 0.95},
     title_font_size=30,
 )
-# save the figure
-save_path = repo_root() / "paper" / "Figures" / f"{study.study_name}.png"
-# fig.write_image(str(save_path))
-fig.show()
-# fig = vis.plot_optimization_history(study)
-# fig.show()
+
+# Create SVG strings directly in memory
+slice_fig = vis.plot_slice(study, target_name="Final forget loss")
+slice_fig.update_layout(**layout)
+# # save slice_fig
+# path = repo_root() / "paper" / "Figures" / f"{study.study_name}_slice.svg"
+# slice_fig.write_image(path)
 
 # %%
-# ! rerun the best trial, with more steps
-trials = study.get_trials()
-finished_trials = [t for t in trials if t.state == optuna.trial.TrialState.COMPLETE]
-worst_to_best = sorted(finished_trials, key=lambda t: t.value)
-
-# config.unlearn_steps = 300
-best_params = deepcopy(worst_to_best[-1].params)
-# best_params["unlearn_lr"] *= 0.3
-# best_params["ret_lora_lr"] = 0.001
-# best_params["quantile"] = 0.007
+opt_history_fig = vis.plot_optimization_history(study)
+opt_history_fig.update_layout(**layout, width=slice_fig.layout.width)
+# # save opt_history_fig
+# path = repo_root() / "paper" / "Figures" / f"{study.study_name}_opt_history.svg"
+# opt_history_fig.write_image(path)
 
 # %%
-best_params
-# %%
-worst_to_best[-1].value
-# %%
+# # ! rerun the best trial, with more steps
+# trials = study.get_trials()
+# finished_trials = [t for t in trials if t.state == optuna.trial.TrialState.COMPLETE]
+# worst_to_best = sorted(finished_trials, key=lambda t: t.value)
 
-config.relearn_steps = 1000
-i = 0
-while i < 1:
-    try:
-        objective(MockTrial(best_params))
-        i += 1
-    except optuna.TrialPruned:
-        pass
-
-# %%
-
+# # config.unlearn_steps = 300
+# best_params = deepcopy(worst_to_best[-1].params)
+# # best_params["unlearn_lr"] *= 0.3
+# # best_params["ret_lora_lr"] = 0.001
+# # best_params["quantile"] = 0.007
+# best_params
+# worst_to_best[-1].value
+# config.relearn_steps = 1000
+# i = 0
+# while i < 1:
+#     try:
+#         objective(MockTrial(best_params))
+#         i += 1
+#     except optuna.TrialPruned:
+#         pass
 # # %%
 # best_model_path = repo_root() / "models" / "best_model.pt"
 # best_model = AutoModelForCausalLM.from_pretrained(config.model_id)
