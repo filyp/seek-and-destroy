516ba3f7cf0501ed6d14a57e6f04656d6538d2a8
Filip Sondej <26285707+filyp@users.noreply.github.com>
2025-01-11 11:58:21 +0100

    mock attention mask; simplify

--- src/unlearning_methods/circuit_breakers.py
+++ src/unlearning_methods/circuit_breakers.py
@@ -24,37 +24,36 @@ def compute_loss(
     step, model, forget_inputs, retain_inputs, target_layers, alpha, config
 ):
 
-    # === retain ===
-    retain_input_ids = retain_inputs.get(f"input_ids")
-    retain_attention_mask = retain_inputs.get(f"attention_mask")
-    # ==== cb ====
-    circuit_breaker_input_ids = forget_inputs.get(f"input_ids")
-    circuit_breaker_attention_mask = forget_inputs.get(f"attention_mask")
+    # # === retain ===
+    # retain_input_ids = retain_inputs.get(f"input_ids")
+    retain_attention_mask = pt.ones_like(retain_inputs)
+    # # ==== cb ====
+    # forget_input_ids = forget_inputs.get(f"input_ids")
+    forget_attention_mask = pt.ones_like(forget_inputs)
+
+    assert forget_attention_mask.shape == retain_attention_mask.shape
 
     # ==== Forward Inputs ====
-    module = "hidden_states"
     retain_inputs = dict(
-        input_ids=retain_input_ids,
+        input_ids=retain_inputs,
         attention_mask=retain_attention_mask,
         output_hidden_states=True,
     )
-    cb_inputs = dict(
-        input_ids=circuit_breaker_input_ids,
-        attention_mask=circuit_breaker_attention_mask,
+    forget_inputs = dict(
+        input_ids=forget_inputs,
+        attention_mask=forget_attention_mask,
         output_hidden_states=True,
     )
 
-    # Those are pretty much arbitrary, the important thing is that retain_coeff increases as the training progresses and circuit_breaker_coeff decreases.
+    # Those are pretty much arbitrary, the important thing is that retain_coeff increases as the training progresses and forget_coeff decreases.
 
-    retain_coeff = alpha * (step / (1 + 2 * config.unlearn_steps))
-    circuit_breaker_coeff = alpha * (1 - (step / (1 + 2 * config.unlearn_steps)))
+    retain_coeff = alpha * (step / (2 * config.unlearn_steps))
+    forget_coeff = alpha * (1 - (step / (2 * config.unlearn_steps)))
 
-    print(
-        f"retain_coeff: {retain_coeff:.4f} || circuit_breaker_coeff: {circuit_breaker_coeff:.4f}"
-    )
+    # print(f"retain_coeff: {retain_coeff:.4f} || forget_coeff: {forget_coeff:.4f}")
 
     # ===== loss components =====
-    layers_circuit_breaker_attention_mask = circuit_breaker_attention_mask.repeat(
+    layers_forget_attention_mask = forget_attention_mask.repeat(
         len(target_layers), 1, 1
     ).unsqueeze(-1)
 
@@ -63,7 +62,7 @@ def compute_loss(
         with pt.no_grad():
             # Retain control
             if retain_coeff > 0:
-                orig_retain_outputs = model(**retain_inputs)[module]
+                orig_retain_outputs = model(**retain_inputs).hidden_states
                 orig_retain_hidden = pt.stack(orig_retain_outputs).detach()
                 layers_retain_attention_mask = retain_attention_mask.repeat(
                     len(orig_retain_outputs), 1, 1
@@ -74,20 +73,20 @@ def compute_loss(
                 gc.collect()
 
             # Circuit Breaker control
-            if circuit_breaker_coeff > 0:
-                circuit_breaker_outputs = model(**cb_inputs)[module]
-                circuit_breaker_hidden = pt.stack(
-                    [circuit_breaker_outputs[l].detach() for l in target_layers]
+            if forget_coeff > 0:
+                forget_outputs = model(**forget_inputs).hidden_states
+                forget_hidden = pt.stack(
+                    [forget_outputs[l].detach() for l in target_layers]
                 )
 
-                del circuit_breaker_outputs
+                del forget_outputs
                 gc.collect()
 
     model.train()
 
     # Retain control
     if retain_coeff > 0:
-        lora_retain_outputs = model(**retain_inputs)[module]
+        lora_retain_outputs = model(**retain_inputs).hidden_states
         lora_retain_hidden = (
             pt.stack(lora_retain_outputs) * layers_retain_attention_mask
         )
@@ -96,27 +95,27 @@ def compute_loss(
         ).nanmean()
 
     # Circuit Breaker control
-    if circuit_breaker_coeff > 0:
-        lora_circuit_breaker_outputs = model(**cb_inputs)[module]
-        lora_circuit_breaker_hidden = pt.stack(
-            [lora_circuit_breaker_outputs[l] for l in target_layers]
-        )
+    if forget_coeff > 0:
+        lora_forget_outputs = model(**forget_inputs).hidden_states
+        lora_forget_hidden = pt.stack([lora_forget_outputs[l] for l in target_layers])
 
-        normalized_lora_circuit_breaker_outputs = lora_circuit_breaker_hidden / (
-            pt.norm(lora_circuit_breaker_hidden, dim=-1, keepdim=True, dtype=pt.float)
+        normalized_lora_forget_outputs = lora_forget_hidden / (
+            pt.norm(lora_forget_hidden, dim=-1, keepdim=True, dtype=pt.float)
         )
-        normalized_circuit_breaker_outputs = circuit_breaker_hidden / (
-            pt.norm(circuit_breaker_hidden, dim=-1, keepdim=True, dtype=pt.float)
+        normalized_forget_outputs = forget_hidden / (
+            pt.norm(forget_hidden, dim=-1, keepdim=True, dtype=pt.float)
         )
         inner_product = (
-            normalized_lora_circuit_breaker_outputs * normalized_circuit_breaker_outputs
-        ) * layers_circuit_breaker_attention_mask
-        circuit_breaker_loss = (
+            normalized_lora_forget_outputs * normalized_forget_outputs
+        ) * layers_forget_attention_mask
+        forget_loss = (
             pt.relu(inner_product.sum(dim=-1)).sum()
-            / layers_circuit_breaker_attention_mask.sum()
+            / layers_forget_attention_mask.sum()
         )
 
-    loss = retain_coeff * retain_loss + circuit_breaker_coeff * circuit_breaker_loss
+    loss = retain_coeff * retain_loss + forget_coeff * forget_loss
+    # # log all losses
+    # logging.info(f"retain_loss: {retain_loss:.4f} || forget_loss: {forget_loss:.4f}")
 
     return loss
 
@@ -135,6 +134,7 @@ def unlearning_func(
     alpha = trial.suggest_float("alpha", 0.1, 2.0)
 
     # Choose between training on 1/2 or 1/3 of middle layers
+    # todo actually use just some layers
     use_half = trial.suggest_categorical(
         "use_half", [True, False]
     )  # True = 1/2, False = 1/3
@@ -149,7 +149,6 @@ def unlearning_func(
     target_layers = list(range(start_layer, start_layer + num_target_layers))
 
     # Add LoRA
-
     ret_lora_config = dict(lora_dropout=0.05, target_modules="all-linear")
 
     ret_lora_c = LoraConfig(r=ret_lora_rank, **ret_lora_config)
