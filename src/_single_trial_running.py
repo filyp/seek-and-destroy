d677a8ab9a41bc5dd2758003f3ab502d3b7395d8
MikolajKniejski <mikolajkniejski@tutanota.com>
2025-01-24 17:14:04 +0100

    Revert "code for single trial running"
    This reverts commit eac05404f33783e87b296dafff210ba42dc09d95.

--- src/_single_trial_running.py
+++ src/_single_trial_running.py
@@ -1,48 +1,32 @@
 # %%
-# %load_ext autoreload
-# %autoreload 2
-from utils.training import MockTrial, eval_, run_study, set_seeds
-from utils.plots_and_stats import plot_slice_layout
-from utils.model_operations import relearn
-from utils.git_and_reproducibility import is_repo_clean
-from utils.data_loading import CachedBatches, dataset_loaders
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch as pt
-import numpy as np
-from types import SimpleNamespace
-from pathlib import Path
-import logging
-import hashlib
+from IPython import get_ipython
+
+# automatically reload all modules
+ipython = get_ipython()
+if ipython is not None:  # Only runs in IPython environment
+    ipython.run_line_magic("load_ext", "autoreload")
+    ipython.run_line_magic("autoreload", "2")
+
+# %%
+# necessary for determinism:
 import os
 
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-# os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':16:8'  # less mem but slower
+# os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":16:8"  # less mem but slower
 
+import logging
+from types import SimpleNamespace
 
-# %%
-config = SimpleNamespace(
-    method_name="seek_and_destroy",
-    # method_name="negative_entropy",
-    # method_name="circuit_breakers",
-    loss_fn_name="neg_cross_entropy",
-    # Model/data configs
-    model_id="EleutherAI/pythia-14m",
-    retain_set_name="wikitext",
-    forget_set_name="python",
-    # retain_set_name="beaver_safe",
-    # forget_set_name="cruelty",
-    # Training constants
-    unlearn_steps=100,
-    batch_size=16,
-    n_trials=100,
-)
-relearn_config = SimpleNamespace(
-    relearn_steps=50,
-    relearn_lr=3e-4,
-    relearn_lora_conf=dict(target_modules="all-linear"),
-)
+import torch as pt
+import yaml
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-pt.set_default_device("cuda")
+from unlearning_methods.tar_masked import tar_masked
+from unlearning_methods.tar_masked_lora import tar_masked_lora
+from utils.data_loading import CachedBatches, dataset_loaders
+from utils.git_and_reproducibility import *
+from utils.model_operations import relearn
+from utils.training import *
 
 logging.basicConfig(
     level=logging.INFO,
@@ -51,7 +35,29 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
+# %%
+# load YAML configuration
+config_path = repo_root() / "configs" / "pythia_ablation.yaml"
+with open(config_path, "r") as f:
+    full_config = yaml.safe_load(f)
+
+hyperparam_ranges = full_config["hyperparams"]
+config = full_config["general_config"]
+
+config = SimpleNamespace(**config)
+relearn_config = SimpleNamespace(**full_config["relearn_config"])
+
+# custom
+# config.train_adversary = False
+
+# %%
+
+print(f"{hyperparam_ranges=}")
+
+pt.set_default_device("cuda")
+
 # load datasets
+set_seeds(42)
 tokenizer = AutoTokenizer.from_pretrained(config.model_id)
 retain_set = dataset_loaders[config.retain_set_name](tokenizer)
 forget_set = dataset_loaders[config.forget_set_name](tokenizer)
@@ -62,32 +68,39 @@ forget_val_batches = CachedBatches(forget_set["validation"], config.batch_size)
 r_eval = next(iter(retain_val_batches))
 f_eval = next(iter(forget_val_batches))
 
-res = eval_(AutoModelForCausalLM.from_pretrained(
-    config.model_id), f_eval, r_eval)
-allowed_f_loss = res["retain_loss"] + 0.1
+allowed_f_loss = eval_(
+    AutoModelForCausalLM.from_pretrained(config.model_id), f_eval, r_eval
+)["retain_loss"]
 
-# %%
-assert config.method_name.replace("_", "").isalpha()
-exec(f"from unlearning_methods.{config.method_name} import unlearning_func")
+unlearning_func = dict(
+    tar_masked_lora=tar_masked_lora,
+    tar_masked=tar_masked,
+)[config.method_name]
+
+
+# construct hyperparams
+hyperparams = {
+    hp_name: (low + high) / 2 for hp_name, (low, high, log) in hyperparam_ranges.items()
+}
+hyperparams = SimpleNamespace(**hyperparams)
 
 set_seeds(42)
-params = MockTrial(
-    # ret_lora_rank=4,
-    # alpha=1,
-    # use_half=True,
-    f_quantile=0.3,
-    r_quantile=0.2,
-    retaining_rate=0.0003,
-    unlearning_rate=0.0003,
-    disruption_score_decay=0.95,
-    pos_grad_discard_factor=1,
-    retain_consistency=0.4,
-)
 model = unlearning_func(
-    params, config, retain_batches, forget_batches, f_eval, r_eval, allowed_f_loss
+    hyperparams,
+    config,
+    retain_batches,
+    forget_batches,
+    f_eval,
+    r_eval,
+    allowed_f_loss,
 )
 
 # %%
-forget_losses = relearn(model, relearn_config,
-                        retain_val_batches, forget_val_batches)
-print(min(forget_losses))
+
+set_seeds(42)
+forget_losses = relearn(
+    model, relearn_config, retain_val_batches, forget_val_batches
+)
+
+# %%
+
