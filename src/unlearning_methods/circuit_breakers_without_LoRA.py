f0ef6ecce93d5d22c25320b4b3bf492c5768a76a
MikolajKniejski <mikolajkniejski@tutanota.com>
2025-01-24 16:21:51 +0100

    Add CB without LoRA

--- src/unlearning_methods/circuit_breakers_without_LoRA.py
+++ src/unlearning_methods/circuit_breakers_without_LoRA.py
@@ -1,21 +1,19 @@
 import gc
 import logging
+from copy import deepcopy
 
 import torch as pt
-from peft import LoraConfig, get_peft_model
-from torch.nn.functional import cosine_similarity
 from transformers import AutoModelForCausalLM
 
 from utils.training import eval_
 
 
 def compute_loss(
-    step, model, forget_inputs, retain_inputs, target_layers, alpha, config
+    step, model, frozen_model, forget_inputs, retain_inputs, target_layers, alpha, config
 ):
-
-    # # === retain ===
+    # === retain ===
     retain_attention_mask = pt.ones_like(retain_inputs)
-    # # ==== cb ====
+    # ==== cb ====
     forget_attention_mask = pt.ones_like(forget_inputs)
 
     assert forget_attention_mask.shape == retain_attention_mask.shape
@@ -33,7 +31,6 @@ def compute_loss(
     )
 
     # Those are pretty much arbitrary, the important thing is that retain_coeff increases as the training progresses and forget_coeff decreases.
-
     retain_coeff = alpha * (step / (2 * config.unlearn_steps))
     forget_coeff = alpha * (1 - (step / (2 * config.unlearn_steps)))
 
@@ -42,30 +39,29 @@ def compute_loss(
         len(target_layers), 1, 1
     ).unsqueeze(-1)
 
-    with model.disable_adapter():
-        model.eval()
-        with pt.no_grad():
-            # Retain control
-            if retain_coeff > 0:
-                orig_retain_outputs = model(**retain_inputs).hidden_states
-                orig_retain_hidden = pt.stack(orig_retain_outputs).detach()
-                layers_retain_attention_mask = retain_attention_mask.repeat(
-                    len(orig_retain_outputs), 1, 1
-                ).unsqueeze(-1)
-                orig_retain_hidden *= layers_retain_attention_mask
+    frozen_model.eval()
+    with pt.no_grad():
+        # Retain control
+        if retain_coeff > 0:
+            orig_retain_outputs = frozen_model(**retain_inputs).hidden_states
+            orig_retain_hidden = pt.stack(orig_retain_outputs).detach()
+            layers_retain_attention_mask = retain_attention_mask.repeat(
+                len(orig_retain_outputs), 1, 1
+            ).unsqueeze(-1)
+            orig_retain_hidden *= layers_retain_attention_mask
 
-                del orig_retain_outputs
-                gc.collect()
+            del orig_retain_outputs
+            gc.collect()
 
-            # Circuit Breaker control
-            if forget_coeff > 0:
-                forget_outputs = model(**forget_inputs).hidden_states
-                forget_hidden = pt.stack(
-                    [forget_outputs[l].detach() for l in target_layers]
-                )
+        # Circuit Breaker control
+        if forget_coeff > 0:
+            forget_outputs = frozen_model(**forget_inputs).hidden_states
+            forget_hidden = pt.stack(
+                [forget_outputs[l].detach() for l in target_layers]
+            )
 
-                del forget_outputs
-                gc.collect()
+            del forget_outputs
+            gc.collect()
 
     model.train()
 
@@ -107,27 +103,23 @@ def compute_loss(
 def unlearning_func(
     trial, config, retain_batches, forget_batches, f_eval, r_eval, allowed_f_loss
 ):
-
     logging.info(f"trial {trial.number} - {trial.params}")
+    logging.info(f"Forget loss - Retain loss")
 
+    # Create main model and frozen copy
     model = AutoModelForCausalLM.from_pretrained(config.model_id)
 
-    # ! parameters
-    ret_lora_rank = trial.suggest_int("ret_lora_rank", 4, 16)
+    frozen_model = deepcopy(model)
+    frozen_model.eval()
+
+    # Parameters
     alpha = trial.suggest_float("alpha", 0.1, 2.0)
-    lr = trial.suggest_float("learning_rate", 1e-4, 1e-2)
+    lr = trial.suggest_float("lr", 1e-4, 1e-2)
 
     num_layers = model.config.num_hidden_layers
     target_layers = [num_layers // 2]
 
-    # Add LoRA
-    ret_lora_config = dict(lora_dropout=0.05, target_modules="all-linear")
-
-    ret_lora_c = LoraConfig(r=ret_lora_rank, **ret_lora_config)
-    model = get_peft_model(
-        model, ret_lora_c, adapter_name="ret_lora", mixed=True)
-
-    optimizer = pt.optim.SGD(model.parameters(), lr=lr)
+    optimizer = pt.optim.Adam(model.parameters(), lr=1e-4)
 
     retain_iter = iter(retain_batches)
     forget_iter = iter(forget_batches)
@@ -136,15 +128,12 @@ def unlearning_func(
         r_input_ids = next(retain_iter)
         f_input_ids = next(forget_iter)
 
-        model.zero_grad(set_to_none=True)
+        model.zero_grad()
         loss = compute_loss(
-            step, model, f_input_ids, r_input_ids, target_layers, alpha, config
+            step, model, frozen_model, f_input_ids, r_input_ids, target_layers, alpha, config
         )
 
         loss.backward()
-        # assert lora is enabled
-        for name, param in model.named_parameters():
-            assert (param.grad is not None) == ("lora" in name), name
         optimizer.step()
 
         # Evaluation
