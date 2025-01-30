fix some missing values in random_mapping, remove gradient accumulation

src/unlearning_methods/random_mapping.py
@@ -1,4 +1,4 @@
-import logging
+from typing import Dict
 
 import torch as pt
 from transformers import AutoModelForCausalLM
@@ -28,61 +28,59 @@ def random_mapping(
         dataloaders["forget_train"],
     )
     """
-    stream_hash_table = torch.randn(
+    stream_hash_table = pt.randn(
         model.config.vocab_size, model.config.hidden_size, requires_grad=False
     )
 
-    for epoch in range(num_epochs):
-        for i in range(max_steps):
-            total_lm_loss = 0
-            total_cos_loss = 0
-            for _ in range(gradient_accumulation_steps):
-                """
-                retain_batch, retain_iterator = get_next_batch(
-                    retain_iterator, retain_dataloader
-                )
-                forget_batch, forget_iterator = get_next_batch(
-                    forget_iterator, forget_dataloader
-                )
-                """
-                retain_batch = next(retain_iterator)
-                forget_batch = next(forget_iterator)
+    # todo later figure out the actual number of forward and backward passes
+    passes_per_loop = 1
+    for loop_num in range(config.unlearn_steps // passes_per_loop):
+        """
+        retain_batch, retain_iterator = get_next_batch(
+            retain_iterator, retain_dataloader
+        )
+        forget_batch, forget_iterator = get_next_batch(
+            forget_iterator, forget_dataloader
+        )
+        """
+        retain_batch = next(retain_iterator)
+        forget_batch = next(forget_iterator)
 
-                lm_loss, cos_loss = random_vector_cosine_obj(
-                    model=model,
-                    x_r=retain_batch,
-                    x_f=forget_batch,
-                    #accelerator=accelerator,
-                    gradient_accumulation_steps=gradient_accumulation_steps,
-                    stream_hash_table=stream_hash_table,
-                    compute_lm_loss=True,
-                )
-                total_lm_loss += lm_loss
-                total_cos_loss += cos_loss
-            optimizer.step()
-            model.zero_grad(set_to_none=True)
- 
+        lm_loss, cos_loss = random_vector_cosine_obj(
+            model=model,
+            x_r=retain_batch,
+            x_f=forget_batch,
+            # accelerator=accelerator,
+            stream_hash_table=stream_hash_table,
+            compute_lm_loss=True,
+        )
+        # total_lm_loss = lm_loss
+        # total_cos_loss = cos_loss
+
+        optimizer.step()
+
+        model.zero_grad(set_to_none=True)
+
     return model
 
+
 def random_vector_cosine_obj(
-    model: torch.nn.Module = None,
-    x_r: Dict[str, torch.Tensor] = None,
-    x_f: Dict[str, torch.Tensor] = None,
-    #accelerator: Accelerator = None,
-    gradient_accumulation_steps: int = None,
-    stream_hash_table: torch.Tensor = None,
+    model: pt.nn.Module = None,
+    x_r: Dict[str, pt.Tensor] = None,
+    x_f: Dict[str, pt.Tensor] = None,
+    # accelerator: Accelerator = None,
+    stream_hash_table: pt.Tensor = None,
     compute_lm_loss: bool = False,
 ) -> int:
     """
     Summary: Maximize cosine similarity between forget and random vectors while maximizing next-token likelihood for the retain set
 
     Args:
-        model (torch.nn.Module): The model to be used for the computation
-        x_r (Dict[str, torch.Tensor]): The retain data
-        x_f (Dict[str, torch.Tensor]): The forget data
+        model (pt.nn.Module): The model to be used for the computation
+        x_r (Dict[str, pt.Tensor]): The retain data
+        x_f (Dict[str, pt.Tensor]): The forget data
         accelerator (Accelerator): The accelerator to be used for the computation
-        gradient_accumulation_steps (int): The number of gradient accumulation steps
-        stream_hash_table (torch.Tensor): The hash table for random vectors
+        stream_hash_table (pt.Tensor): The hash table for random vectors
 
     Returns:
     """
@@ -90,15 +88,14 @@ def random_vector_cosine_obj(
     _x_f = _filter_inputs(x_f)
 
     # (1) Compute LM loss for retain data
-    x_r_lm_loss = torch.tensor(0.0)
+    x_r_lm_loss = pt.tensor(0.0)
     if compute_lm_loss:
         logits = model(**_x_r).logits
         x_r_lm_loss = (
             log_p_loss(logits, x_r.get("labels"), model.vocab_size)
-            / gradient_accumulation_steps
             # * scale
         )
-        #accelerator.backward(x_r_lm_loss)
+        # accelerator.backward(x_r_lm_loss)
         x_r_lm_loss.backward()
 
     # (2) (flatten sequence length and batch size)
@@ -108,19 +105,13 @@ def random_vector_cosine_obj(
     f_input_ids = x_f.get("input_ids").view(-1)
     rand_stream = [stream_hash_table[f_input_ids] for _ in f_stream]
 
-    # maximize cosine similarity between each unit-normalized random vector and each unit-normalized forget stream row (batch_size * sequence_length, hidden_size)
-    cos_sim_loss = (
-        torch.stack(
-            [
-                (
-                    1 - torch.abs(torch.nn.functional.cosine_similarity(f, r, dim=-1))
-                ).mean()
-                for f, r in zip(f_stream, rand_stream)
-            ]
-        ).mean()
-        / gradient_accumulation_steps
-    )
-    #accelerator.backward(cos_sim_loss)
-    cos_sim_loss.backward()    
+    # maximize cosine similarity between each unit-normalized random vector 
+    # and each unit-normalized forget stream row (batch_size * sequence_length, hidden_size)
+    cos_sim_loss = pt.stack([
+        (1 - pt.abs(pt.nn.functional.cosine_similarity(f, r, dim=-1))).mean()
+        for f, r in zip(f_stream, rand_stream)
+    ]).mean()
+    # accelerator.backward(cos_sim_loss)
+    cos_sim_loss.backward()
 
     return x_r_lm_loss.item(), cos_sim_loss.item()